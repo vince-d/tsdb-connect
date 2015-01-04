d12e834a4c978b7dfadb4339486da61f2cfc8788
Vincent Diener <vincent.diener@gmail.com>
2015-01-04 17:29:31 +0100

    Fixed project files and created class for TSDB parameters.

--- app/src/main/java/edu/teco/tsdbwriter/DataPoint.java
+++ app/src/main/java/edu/teco/tsdbwriter/DataPoint.java
@@ -4,7 +4,11 @@ package edu.teco.tsdbwriter;
  * Created by Vincent on 04.01.2015.
  */
 
-// A single data point in a time series.
+
+
+/**
+ * A single data point in a time series.
+ */
 public class DataPoint {
 
     private String value;
---
+++ app/src/main/java/edu/teco/tsdbwriter/TSDB.java
@@ -0,0 +1,7 @@
+package edu.teco.tsdbwriter;
+
+/**
+ * Created by Vincent on 04.01.2015.
+ */
+public class TSDB {
+}
